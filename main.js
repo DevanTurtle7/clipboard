e0ef4cef349c360ecfd71e3da9f29ffd23cc19d1
Devan Kavalchek <dkavalchek@MacBook-Pro.local>
2020-07-04 00:54:12 -0400

    headers againe

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ $(document).ready(function () {
 
     $.ajax({
         dataType: 'json',
+        url: 'https://id.twitch.tv/oauth2/authorize',
+        headers: {
+            'client_id': 'b3f8o6lurrry9l9ymrx865fh798ae1',
+            'response_type': 'code',
+            'redirect_uri' : 'https://devanturtle7.github.io/clipboard/&scope=clips:edit',
+        }
+    })
+
+
+    $.ajax({
+        dataType: 'json',
         url: 'https://api.twitch.tv/helix/users',
         headers: {
-            'Authorization': 'Bearer mytoken'
+            'Authorization': 'Bearer mytoken',
             'Client-Id': 'b3f8o6lurrry9l9ymrx865fh798ae1',
         }
     })
