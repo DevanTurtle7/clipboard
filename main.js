ea0fabb0979eba80b45ee3c569775ca6c399508d
Devan Kavalchek <dkavalchek@MacBook-Pro.local>
2020-07-04 00:59:59 -0400

    trying this again..

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
     console.log('hello')
 
-    $.ajax({
-        dataType: 'json',
-        url: 'https://id.twitch.tv/oauth2/authorize',
-        headers: {
-            'client_id': 'b3f8o6lurrry9l9ymrx865fh798ae1',
-            'response_type': 'code',
-            'redirect_uri' : 'https://devanturtle7.github.io/clipboard/&scope=clips:edit',
-        }
+    $.get("https://id.twitch.tv/oauth2/authorize?client_id=b3f8o6lurrry9l9ymrx865fh798ae1&response_type=code&redirect_uri=https://devanturtle7.github.io/clipboard/&scope=clips:edit", function(data) {
+        console.log(data)
     })
 
 
