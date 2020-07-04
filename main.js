43943da500dac621fc2ad5e2726d87ccac34676c
Devan Kavalchek <dkavalchek@MacBook-Pro.local>
2020-07-04 00:46:35 -0400

    ajax test

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     console.log('hello')
 
-    $.get('https://api.twitch.tv/helix/users?client_id=b3f8o6lurrry9l9ymrx865fh798ae1', function (data) {
-        console.log(data);
+    $.ajax({
+        dataType: 'json',
+        url: 'https://api.twitch.tv/helix/users',
+        headers: {
+            'Client-Id': 'b3f8o6lurrry9l9ymrx865fh798ae1',
+            'token_type': 'bearer'
+        }
     })
 });
