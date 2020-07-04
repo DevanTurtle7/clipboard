712d29fb1eb295cf4e606008cc80d80b11e91112
Devan Kavalchek <dkavalchek@MacBook-Pro.local>
2020-07-04 00:53:57 -0400

    fixed headers

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
         dataType: 'json',
         url: 'https://api.twitch.tv/helix/users',
         headers: {
+            'Authorization': 'Bearer mytoken'
             'Client-Id': 'b3f8o6lurrry9l9ymrx865fh798ae1',
-            'token_type': 'bearer'
         }
     })
 });
