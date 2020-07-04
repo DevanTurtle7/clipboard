3468f88f70ec9410c0c06eb921a1bc46f7a4f990
Devan Kavalchek <dkavalchek@MacBook-Pro.local>
2020-07-04 00:20:54 -0400

    forgot script src

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ var code = fetch('https://api.twitch.tv/kraken/user')
 
 console.log('hello')
 console.log(code)
+
+fetch('http://api.twitch.tv/kraken/user')
+  .then(response => response.json())
+  .then(data => console.log(data));
