a69af572ad4b77a331a15cdf2f93a1cadf9cd11c
Devan Kavalchek <dkavalchek@MacBook-Pro.local>
2020-07-04 00:33:37 -0400

    fixing jquery

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-console.log('hello')
+$(document).ready(function () {
+    console.log('hello')
 
-$.get('https://api.twitch.tv/helix/users?client_id=b3f8o6lurrry9l9ymrx865fh798ae1', function(data) {
-    console.log(data);
-})
+    $.get('https://api.twitch.tv/helix/users?client_id=b3f8o6lurrry9l9ymrx865fh798ae1', function (data) {
+        console.log(data);
+    })
+});
